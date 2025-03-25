b2fa54200b4727dc2a2bb468e8a617ab856956f1
Pedro Reis Domingos <pedro.domingos2004@gmail.com>
2025-03-25 12:23:55 -0300

    Finalização de sistema, com mudança e seleção de canais, volume  e ligar ou desligar TV

--- tipo-variaveis/src/Usuario.java
+++ tipo-variaveis/src/Usuario.java
@@ -1,16 +1,27 @@
 public class Usuario {
     public static void main(String[] args) throws Exception {
-        
+
         SmartTv smartTv = new SmartTv();
 
-        System.out.println("TV está ligada? " +smartTv.ligada);
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        
+        System.out.println("canal atual : " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("TV está ligada? " + smartTv.ligada);
         System.out.println("Canal atual: " + smartTv.canal);
         System.out.println("volume atual: " + smartTv.volume);
-    
+
         smartTv.ligar();
-        System.out.println("TV está ligada? " +smartTv.ligada);
+        System.out.println("TV está ligada? " + smartTv.ligada);
 
         smartTv.desligar();
-        System.out.println("TV está ligada? " +smartTv.ligada);
+        System.out.println("TV está ligada? " + smartTv.ligada);
     }
 }
