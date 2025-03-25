40f7507e21678dcd320068241aeb7f72f4f2a9af
Pedro Reis Domingos <pedro.domingos2004@gmail.com>
2025-03-25 11:25:52 -0300

    Projeto básico SmartTv utilizando métodos

---
+++ tipo-variaveis/src/Usuario.java
@@ -0,0 +1,16 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV está ligada? " +smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("volume atual: " + smartTv.volume);
+    
+        smartTv.ligar();
+        System.out.println("TV está ligada? " +smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("TV está ligada? " +smartTv.ligada);
+    }
+}
