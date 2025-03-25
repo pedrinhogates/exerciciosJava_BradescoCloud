2c7fb13e8fe134eec65f73a22018f0b489f5b9a0
Pedro Reis Domingos <pedro.domingos2004@gmail.com>
2025-03-25 08:12:20 -0300

    Testando variáveis e alguns operadores

--- tipo-variaveis/src/App.java
+++ tipo-variaveis/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String meuNome = "Pedro Reis";
+        int ContaBancaria = 7653;
+        double saldoBancario = 1458.55;
+        int chequeEspecial = 450;
+
+            saldoBancario = saldoBancario - 1000;
+            System.out.println(saldoBancario);
+
+            saldoBancario = saldoBancario + chequeEspecial;
+                System.out.println(saldoBancario);  
+            }
     }
-}
+
