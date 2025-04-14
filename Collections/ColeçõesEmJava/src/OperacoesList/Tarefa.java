5e67366a609068725527cbcddf2170fcd67b5e9c
Pedro Reis Domingos <pedro.domingos2004@gmail.com>
2025-04-14 15:00:08 -0300

    Adiciona classes Tarefa e listaTarefa para gerenciamento de tarefas, incluindo métodos para adicionar, remover e listar tarefas.

--- Collections/ColeçõesEmJava/src/OperacoesList/Tarefa.java
+++ Collections/ColeçõesEmJava/src/OperacoesList/Tarefa.java
@@ -1,4 +1,4 @@
-package OperaçõesList;
+package OperacoesList;
 
 public class Tarefa {
 
--- Collections/ColeçõesEmJava/src/OperacoesList/listaTarefa.java
+++ Collections/ColeçõesEmJava/src/OperacoesList/listaTarefa.java
@@ -1,4 +1,4 @@
-package OperaçõesList;
+package OperacoesList;
 
 import java.util.ArrayList;
 import java.util.List;
