abc96d40ebb6e1388b92c0e12ea8be13ad3b7bd5
Pedro Reis Domingos <pedro.domingos2004@gmail.com>
2025-04-15 15:29:39 -0300

    Adiciona classe OrdenacaoPessoas para gerenciamento de listas de pessoas, incluindo métodos para adicionar e ordenar por idade e altura.

---
+++ Collections/ColeçõesEmJava/src/OrdenacaoList/OrdenacaoPessoas.java
@@ -0,0 +1,30 @@
+package OrdenacaoList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    private List<Pessoa> pessoasList;
+
+    public OrdenacaoPessoas() {
+        this.pessoasList = new ArrayList<>();
+    }
+    
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoasList.add(new Pessoa (nome, idade, altura));
+    }
+
+    public List <Pessoa> ordenarPorIdade() {
+            List <Pessoa> pessoasOrdenadas = new ArrayList<>(pessoasList);
+            Collections.sort(pessoasOrdenadas);
+            return pessoasOrdenadas;
+            
+    }   
+    public List <Pessoa> ordenarPorAltura(){
+        List <Pessoa> pessoasOrdenadas = new ArrayList<>(pessoasList);
+         Collections.sort(pessoasOrdenadas, new CompartorPorAltura());
+        return pessoasOrdenadas;
+    }
+    }
+
