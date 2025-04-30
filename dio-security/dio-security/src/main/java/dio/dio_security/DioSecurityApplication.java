bf983ab2da41b4c8b55d13cc1fbbe1c2bba5bead
Pedro Reis Domingos <pedro.domingos2004@gmail.com>
2025-04-30 16:14:02 -0300

    Subindo projeto aplicando Spring security, testando propiedades da aplicação e configurando user name e user password, para acesso.

---
+++ dio-security/dio-security/src/main/java/dio/dio_security/DioSecurityApplication.java
@@ -0,0 +1,13 @@
+package dio.dio_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSecurityApplication.class, args);
+	}
+
+}
---
+++ dio-security/dio-security/src/test/java/dio/dio_security/DioSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.dio_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
